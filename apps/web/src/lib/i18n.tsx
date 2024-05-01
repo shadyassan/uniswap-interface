d36cec8db7e4a736d7440af3ae35227a9f9f421f
Shady <shady.assan@gmail.com>
2024-05-01 22:04:19 +0300

    added messages

--- apps/web/src/lib/i18n.tsx
+++ apps/web/src/lib/i18n.tsx
@@ -1,18 +1,22 @@
 import { i18n } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
-import { DEFAULT_LOCALE, SupportedLocale } from 'constants/locales';
+import {
+  DEFAULT_LOCALE,
+  DEFAULT_MESSAGES,
+  SupportedLocale,
+} from 'constants/locales';
 import { ReactNode, useEffect } from 'react';
 
 // Initialize the locale immediately to DEFAULT_LOCALE/DEFAULT_MESSAGES,
 // so that messages are shown while the appropriate translation load.
 // This is necessary for initial macro translations (t``) to work in the DEFAULT_LOCALE.
-// i18n.load(DEFAULT_LOCALE, DEFAULT_MESSAGES);
+i18n.load(DEFAULT_LOCALE, DEFAULT_MESSAGES);
 i18n.activate(DEFAULT_LOCALE);
 
 export async function dynamicActivate(locale: SupportedLocale) {
-  // if (i18n.locale === locale) return;
+  if (i18n.locale === locale) return;
   try {
-    const catalog = await import(`locales/en-US.js`);
+    const catalog = await import(`locales/${locale}.js`);
     // Bundlers will either export it as default or as a named export named default.
     i18n.load(locale, catalog.messages || catalog.default.messages);
   } catch (error: unknown) {
