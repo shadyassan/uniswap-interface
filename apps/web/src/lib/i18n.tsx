ee21eb4f4a62bab54b17397f54b66428c9a26024
Shady <shady.assan@gmail.com>
2024-05-01 23:34:55 +0300

    fix dynamicActivate function

--- apps/web/src/lib/i18n.tsx
+++ apps/web/src/lib/i18n.tsx
@@ -3,17 +3,17 @@ import { I18nProvider } from '@lingui/react';
 import { DEFAULT_LOCALE, SupportedLocale } from 'constants/locales';
 import { ReactNode, useEffect } from 'react';
 
-import { messages } from 'locales/en-US';
+// import { messages } from 'locales/en-US';
 
 // Initialize the locale immediately to DEFAULT_LOCALE/DEFAULT_MESSAGES,
 // so that messages are shown while the appropriate translation load.
 // This is necessary for initial macro translations (t``) to work in the DEFAULT_LOCALE.
 
-i18n.load(DEFAULT_LOCALE, messages);
-i18n.activate(DEFAULT_LOCALE);
+// i18n.load(DEFAULT_LOCALE, messages);
+// i18n.activate(DEFAULT_LOCALE);
 
 export async function dynamicActivate(locale: SupportedLocale) {
-  if (i18n.locale === locale) return;
+  // if (i18n.locale === locale) return;
   try {
     const catalog = await import(`locales/${locale}.js`);
     // Bundlers will either export it as default or as a named export named default.
@@ -33,7 +33,7 @@ interface ProviderProps {
 export function Provider({ locale, onActivate, children }: ProviderProps) {
   useEffect(() => {
     dynamicActivate(locale)
-      .then(() => onActivate?.(locale))
+      // .then(() => onActivate?.(locale))
       .catch((error) => {
         console.error('Failed to activate locale', locale, error);
       });
